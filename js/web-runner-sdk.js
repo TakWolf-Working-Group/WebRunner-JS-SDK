66905d79379eea29d5f28e908943ce948961cc27
TakWolf <takwolf@foxmail.com>
2023-09-27 22:03:06 +0800

    update

--- js/web-runner-sdk.js
+++ js/web-runner-sdk.js
@@ -2,12 +2,44 @@
  * WebRunner - JS SDK
  */
 (function () {
+    let jsBridgeApi = {
+        runtimeInfo: {
+            platform: 'web',
+            isNative: false,
+            version: {
+                name: '0.0.0',
+                code: 0,
+            },
+        },
+        showToast(message) {
+            console.warn('Not supported');
+        },
+    };
 
+    function createApiForAndroid(jsBridgeApi, androidJsBridgeApi) {
+        window.androidJsBridgeCallback = {
+            // TODO
+        };
 
+        jsBridgeApi.runtimeInfo = {
+            platform: 'android',
+            isNative: true,
+            version: {
+                name: androidJsBridgeApi.getVersionName(),
+                code: androidJsBridgeApi.getVersionCode(),
+            },
+        };
 
+        jsBridgeApi.showToast = function (message) {
+            androidJsBridgeApi.showToast(message);
+        };
 
+        return jsBridgeApi;
+    }
 
-
-
-
+    if (!!window.androidJsBridgeApi) {
+        window.jsBridgeApi = createApiForAndroid(jsBridgeApi, window.androidJsBridgeApi);
+    } else {
+        window.jsBridgeApi = jsBridgeApi;
+    }
 })()
