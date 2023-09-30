f03429dc2f750927417b3c674e87384a102cc104
TakWolf <takwolf@foxmail.com>
2023-09-30 12:34:47 +0800

    update

---
+++ js/web-runner.js
@@ -0,0 +1,25 @@
+(function () {
+    if (window.webRunner) {
+        return;
+    }
+
+    if (!window.jsBridge) {
+        return;
+    }
+    let jsBridge = window.jsBridge;
+
+    window.webRunner = {
+        showToast(message) {
+            jsBridge.callService('showToast', message).then()
+        },
+        openShare(data) {
+            jsBridge.callService('openShare', JSON.stringify(data)).then()
+        },
+        checkPermission(permission) {
+            return jsBridge.callService('checkPermission', permission).then((isGranted) => isGranted === 'true')
+        },
+        requestPermission(permission) {
+            return jsBridge.callService('requestPermission', permission).then((isGranted) => isGranted === 'true')
+        },
+    };
+})();
